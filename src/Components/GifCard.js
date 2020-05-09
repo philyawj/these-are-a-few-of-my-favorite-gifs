52cddf6612b501de32eb214cfbfdb36cdd92aee2
Jordan Philyaw <jordan@gofundme.com>
2020-05-09 11:53:54 -0700

    image path for github

--- src/Components/GifCard.js
+++ src/Components/GifCard.js
@@ -6,7 +6,7 @@ class GifCard extends React.Component {
     render() {
         return (
             <Card key={this.props.index} className="bg-dark text-white">
-                <Card.Img className="gif-img" alt={this.props.gif.alt} src={'/assets/gifs/' + this.props.gif.name + '.gif'} />
+                <Card.Img className="gif-img" alt={this.props.gif.alt} src={'./assets/gifs/' + this.props.gif.name + '.gif'} />
                 <Card.Body>
                     <Card.Text>
                         {this.props.gif.tags.map((tag, index) =>
