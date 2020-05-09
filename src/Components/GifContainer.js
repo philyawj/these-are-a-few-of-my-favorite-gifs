4c14c945421661d8bc1b22a811c8572633562da5
Jordan Philyaw <jordan@gofundme.com>
2020-05-08 21:06:31 -0700

    clickable badges for top tag flitering

--- src/Components/GifContainer.js
+++ src/Components/GifContainer.js
@@ -3,13 +3,15 @@ import '../App.css';
 import data from '../gifs.json'
 import CardColumns from 'react-bootstrap/CardColumns';
 import Form from 'react-bootstrap/Form';
+import Badge from 'react-bootstrap/Badge';
 import GifCard from './GifCard';
 
 class GifContainer extends React.Component {
 
     state = {
         inputValue: '',
-        gifs: []
+        gifs: [],
+        topTags: []
     };
 
     componentDidMount() {
@@ -35,6 +37,13 @@ class GifContainer extends React.Component {
         this.setState({ inputValue: str.substr(1, str.length - 2) })
     };
 
+    handlePillClick = (event) => {
+        // clicking tag under gif sets search bar
+        let str = event.target.textContent;
+        // substr removes front # and ending space
+        this.setState({ inputValue: str.substr(1, str.length - 1) })
+    };
+
     formPreventDefault(e) {
         e.preventDefault();
     }
@@ -48,10 +57,12 @@ class GifContainer extends React.Component {
         let tagCounts = {};
         oneTagArray.forEach(function (x) { tagCounts[x] = (tagCounts[x] || 0) + 1; });
 
+        // state set to tags in order of highest usage
         let tagSorted = Object.keys(tagCounts);
-        tagSorted.sort(function (a, b) { return tagCounts[b] - tagCounts[a] });
-        console.log(tagSorted.slice(0, 5));
+        tagSorted = tagSorted.sort(function (a, b) { return tagCounts[b] - tagCounts[a] });
+        this.setState({ topTags: tagSorted.slice(0, 10) })
     }
+
 
 
     render() {
@@ -67,6 +78,15 @@ class GifContainer extends React.Component {
                         <Form.Control type="text" value={this.state.inputValue} onChange={this.gifFilterOnChange} />
                     </Form.Group>
                 </Form>
+
+                <div>
+                    <h4>The top tags go here</h4>
+                    {this.state.topTags.map((tag, index) =>
+                        <Badge key={index} variant="primary" onClick={(e) => this.handlePillClick(e)}>
+                            #{tag}
+                        </Badge>
+                    )}
+                </div>
 
                 <CardColumns>
                     {filteredGifs.map((gif, index) =>
